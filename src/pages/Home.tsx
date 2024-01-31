d7c4d8cd628e701902ff06b606b3bf8b9826af41
BuurPrime <sofieamaliebuur@gmail.com>
2024-01-31 23:14:38 +0100

    Tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface MyComponentProps {
+  
+}
+
+const Home: FC<MyComponentProps> = ( ) => {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-2xl">
+      <h1>Hsdfsdf</h1>
+    </div>
+  );
+};
+
+export default Home;
