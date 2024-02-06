7aaa1a37dbedcd930ec428408d6dd494a19dabe4
BuurPrime <sofieamaliebuur@gmail.com>
2024-02-06 22:08:30 +0100

    Finished Top Nav Bar and Added About Me Page

--- src/components/TopNavBar.tsx
+++ src/components/TopNavBar.tsx
@@ -5,14 +5,31 @@ const TopBarNav: FC = () => {
   return (
     <div className="top-nav-bar-container">
       <p className="name-logo-top-left"> SOFIE BUUR </p>
-      <hr className="horizontal-line"/>
+      <hr className="horizontal-line" />
       <div className="navigation-menu">
         <p className="navigation-menu-item"> WORK </p>
         <p className="navigation-menu-item"> ABOUT ME </p>
         <p className="navigation-menu-item"> CONTACT </p>
       </div>
-      <div>
-        <img src='../images/linkedin.png' alt="" width="20" height="20"/>
+      <div className="platform-links">
+        <a href="https://www.linkedin.com/in/sofie-buur-msc-softwaredesign/">
+          <img
+            src={process.env.PUBLIC_URL + "/images/linkedin.png"}
+            alt=""
+            width="20"
+            height="20"
+            className="platform-icon"
+          />
+        </a>
+        <a href="https://github.com/BuurPrime">
+          <img
+            src={process.env.PUBLIC_URL + "/images/github.png"}
+            alt=""
+            width="20"
+            height="20"
+            className="platform-icon"
+          />
+        </a>
       </div>
     </div>
   );
