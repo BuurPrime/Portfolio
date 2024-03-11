4871546b4d77e119a3b22589084a3c020169fa47
BuurPrime <sofieamaliebuur@gmail.com>
2024-03-11 10:36:13 +0100

    aboutme

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,10 +1,34 @@
 import React, { FC } from "react";
 
 const AboutMe: FC = () => {
-  return <div className="mx-36">
+  return (
+    <div className="mx-36 mt-48">
+      <div className="grid grid-cols-5 grid-rows-3 gap-5">
+        <div className="col-start-2">
+          <h2> Hi there! </h2>
+        </div>
 
-    
-  </div>;
+        <div className="col-start-2 col-span-2">
+          <p className="text-justify">
+            {" "}
+            Thank you for being here - I'm Sofie, an aspiring front-developer
+            with proficiency in UI/UX. I engage in different kinds of creative
+            projects from web design and apps to posters and stickers. I enjoy
+            having fun in the creative process, and to explore different and
+            alternative solutions to otherwise trivial problems.{" "}
+          </p>
+        </div>
+
+        <div className="col-start-4">
+        <img
+            src={process.env.PUBLIC_URL + "/images/me.jpg"}
+            alt=""
+            className=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutMe;
